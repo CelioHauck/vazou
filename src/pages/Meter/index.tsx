b7502190e5c843f1d4b842a6390bdb3b125e494d
Hauck <celiohauck@gmail.com>
2021-09-12 16:37:16 -0300

    Teste de medição

--- src/pages/Meter/index.tsx
+++ src/pages/Meter/index.tsx
@@ -1,19 +1,36 @@
-import { Box, Typography } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { Box, Button, Typography } from '@material-ui/core';
 import ReactSpeedometer, {
   CustomSegmentLabelPosition,
 } from 'react-d3-speedometer';
 
 const Meter = () => {
+  const [quantity, setQuantity] = useState(0);
+
+  useEffect(() => {});
+
+  const handleTeste = () => {
+    setQuantity((old) => {
+      return 800;
+    });
+  };
+
   return (
     <Box p={4}>
       <Box mb={4} flex="1 1 auto">
         <Typography variant="h6">Medição do gás</Typography>
       </Box>
-      <Box width={1} display="flex" alignItems="center" justifyContent="center">
+      <Box
+        width={1}
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        flexDirection="column"
+      >
         <ReactSpeedometer
           customSegmentStops={[0, 80, 750, 1000]}
           segmentColors={['firebrick', 'tomato', 'gold', 'limegreen'].reverse()}
-          value={333}
+          value={quantity}
           currentValueText="Niveis de vazamento!"
           customSegmentLabels={[
             {
@@ -34,6 +51,11 @@ const Meter = () => {
             },
           ].reverse()}
         />
+        <Box>
+          <Button variant="outlined" color="primary" onClick={handleTeste}>
+            Teste
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
